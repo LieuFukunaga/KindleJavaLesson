dc40f42198e745da035558772086a9d447563395
LieuFukunaga <61906903+LieuFukunaga@users.noreply.github.com>
2020-10-21 09:52:45 +0900

    DateCalendarTestクラスのprintCalメソッドに処理を追記しました。
    処理を切り分けてコードを整理しました。

--- KindleJava/src/DateCalendarTest.java
+++ KindleJava/src/DateCalendarTest.java
@@ -35,6 +35,29 @@ public class DateCalendarTest {
 			System.out.println("Invalid Value");
 			break;
 		}
+		ansCal(cal, days);
+		setCal(cal, 2021);
+		setCal(cal, 3333, 11, 22);
+		Calendar cal2 = Calendar.getInstance();
+		System.out.println("cal2 => " + getCalString(cal2));
+		compareCal(cal, cal2);
+		cal.add(Calendar.MONTH, 1);
+		cal.setLenient(true);
+		cal.set(2020, 10, 45);
+		System.out.println("setLenient / true => " + getCalString(cal));
+		cal.setLenient(false);
+		cal.set(2020, 10, 45);
+		try {
+			System.out.println("setLenient / false => " + getCalString(cal));
+		} catch (Exception e) {
+			System.out.println("厳密な解釈をせずにありえない日付をセット＝＞エラー");
+		}
+		
+		
+	}
+	
+	
+	public static void ansCal(Calendar cal, String[] days) {
 		System.out.println("What day of year is it today? => " + cal.get(Calendar.DAY_OF_YEAR) + ".");
 		System.out.println("What day of month is it today? => " + cal.get(Calendar.DAY_OF_MONTH) + ".");
 		System.out.println("What day is it today? => " + days[cal.get(Calendar.DAY_OF_WEEK) - 1]  + ".");
@@ -42,25 +65,35 @@ public class DateCalendarTest {
 		System.out.println("What week of year is it today? => " + cal.get(Calendar.WEEK_OF_YEAR) + ".");
 		System.out.println("What week of month is it today? => " + cal.get(Calendar.WEEK_OF_MONTH) + ".");
 		System.out.println("Print YEAR without constance => " + cal.get(1) );
-		cal.set(Calendar.YEAR, 2021);
-		System.out.println("set YEAR => " + getCalString(cal));
-		cal.set(3333, 11, 22);;
-		System.out.println("set YEAR, MONTH, DATE => " + getCalString(cal));
-		Calendar cal2 = Calendar.getInstance();
-		System.out.println("cal2 => " + getCalString(cal2));
-		boolean compareCal;
-		compareCal = cal.equals(cal2);
-		System.out.println("Is cal equal to cal2? => " + compareCal);
-		compareCal = cal.before(cal2);
-		System.out.println("Is cal before cal2? => " + compareCal);
-		comapreCal = cal.after(cal2);
-		System.out.println("Is cal after cal2? => " + compareCal);
 		
 	}
+
 	
 	public static String getCalString(Calendar tmpCal) {
 		// getメソッドの返り値：int型
 		return String.valueOf(tmpCal.get(Calendar.YEAR) + "/" + String.valueOf(tmpCal.get(Calendar.MONTH) + 1) + "/" + String.valueOf(tmpCal.get(Calendar.DATE)));
+	}
+	
+	
+	public static void setCal(Calendar cal, int year) {
+		cal.set(Calendar.YEAR, year);
+		System.out.println("set YEAR => " + getCalString(cal));
+	}
+	
+	public static void setCal(Calendar cal, int year, int month, int date) {
+		cal.set(year, month, date);;
+		System.out.println("set YEAR, MONTH, DATE => " + getCalString(cal));
+	}
+	
+	
+	public static void compareCal(Calendar cal, Calendar cal2) {
+		boolean compCal;
+		compCal = cal.equals(cal2);
+		System.out.println("Is cal equal to cal2? => " + compCal);
+		compCal = cal.before(cal2);
+		System.out.println("Is cal before cal2? => " + compCal);
+		compCal = cal.after(cal2);
+		System.out.println("Is cal after cal2? => " + compCal);
 	}
 	
 }
